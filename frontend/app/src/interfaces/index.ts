579237965b3d3e3334ec0fdec1344ace390445f4
Marie Yoshida <18l1135h@gmail.com>
2022-10-31 21:41:54 +0900

    [fix]ログイン機能で使う型定義をするため

---
+++ frontend/app/src/interfaces/index.ts
@@ -0,0 +1,21 @@
+export interface SignUpData {
+  name: string;
+  email: string;
+  password: string;
+  passwordConfirmation: string;
+}
+
+export interface SignInData {
+  email: string;
+  password: string;
+}
+
+export interface User {
+  id: number;
+  uid: string;
+  provider: string;
+  email: string;
+  name: string;
+  image?: string;
+  allowPasswordChange: boolean;
+}
